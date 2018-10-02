78dd1e41b2c8a0b5704f958badea66192157aca3
Chris Wilcoxson <chris@slushman.com>
2018-10-02 10:35:21 -0500

    Initial commit.

---
+++ components/EditableTimer.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import TimerForm from './TimerForm';
+import Timer from './Timer';
+
+class EditableTimer extends Component {
+	render() {
+		return (
+			<div>
+				
+			</div>
+		);
+	}
+}
+
+EditableTimer.propTypes = {
+
+};
+
+export default EditableTimer;
